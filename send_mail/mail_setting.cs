d58013ae82349620135066b04d89743965eb40b8
Python_web crawler <31296722+cassieeric@users.noreply.github.com>
2019-05-09 12:52:47 +0800

    Add files via upload

---
+++ send_mail/mail_setting.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace 贺培超
+{
+    public partial class mail_setting : Form
+    {
+        public  string rt = "";
+        public mail_setting()
+        {
+            InitializeComponent();
+        }
+
+        private void mail_setting_Load(object sender, EventArgs e)
+        {
+            richtbxMailContentReview.Text = rt;
+        }
+    }
+}
